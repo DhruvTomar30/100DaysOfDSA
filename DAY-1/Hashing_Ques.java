04ae4a13745ae2d760d53f889243c93d22de671e
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-02 13:59:36 +0530

    Changes in 2nd ques.

---
+++ DAY-1/Hashing_Ques.java
@@ -0,0 +1,81 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Hashing_Ques {
+
+    // 01. Find all the elements that appears more than [n/3] times in Array....
+    // nums[]={1,3,2,5,1,3,1,5,1}, 
+
+    public static void majorityElement(int nums[]){
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        for(int key: map.keySet()){
+            if(map.get(key)>n/3){               // n/3= 9/3=3.
+                System.out.println(key);
+            }
+        }
+    }
+
+
+    // 02. count Frequency of each element in the array..... 
+    public static void CountFreq(int nums[]){
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        // Traversing..
+        for(Map.Entry<Integer,Integer>e: map.entrySet()){
+            System.out.println(e.getKey()+ " "+ e.getValue());
+        }
+    }
+
+
+    // 03. Find the highest/lowest Frequency Element...
+    
+
+
+
+
+
+
+
+
+
+    // Main....
+     public static void main(String args[]){
+        int nums[]={1,3,2,5,1,3,1,5,1};              // 1- 4, 2- 1, 3- 2, 5- 2     key- frequency
+        
+
+        
+        //01. all elements > than n/3
+        // majorityElement(nums);                      // 4>3 so 1 is answer...
+
+
+        //02. Count of freq...
+        CountFreq(nums);                                // 1- 4, 2- 1, 3- 2, 5- 2         key- frequency
+
+
+        // 03. Highest/lowest frequency..
+
+    
+    
+    
+    
+    
+    
+    
+    }
+}
