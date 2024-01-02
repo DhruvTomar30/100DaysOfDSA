eed98dd815e0a176c367e554d4b02776810f05f7
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-02 19:21:08 +0530

    3rd question

--- DAY-1/Hashing_Ques.java
+++ DAY-1/Hashing_Ques.java
@@ -44,7 +44,37 @@ public class Hashing_Ques {
 
 
     // 03. Find the highest/lowest Frequency Element...
-    
+    static void Frequency(int arr[], int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+
+        int maxFreq = 0, minFreq = Integer.MAX_VALUE;
+        int maxEle = 0, minEle = 0;
+        // Traverse through map and find the elements
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int count = entry.getValue();
+            int element = entry.getKey();
+
+            if (count > maxFreq) {
+                maxEle = element;
+                maxFreq = count;
+            }
+            if (count < minFreq) {
+                minEle = element;
+                minFreq = count;
+            }
+        }
+
+        System.out.println("The highest frequency element is: " + maxEle);
+        System.out.println("The lowest frequency element is: " + minEle);
+    }
 
 
 
@@ -65,11 +95,13 @@ public class Hashing_Ques {
 
 
         //02. Count of freq...
-        CountFreq(nums);                                // 1- 4, 2- 1, 3- 2, 5- 2         key- frequency
+        // CountFreq(nums);                                // 1- 4, 2- 1, 3- 2, 5- 2         key- frequency
 
 
         // 03. Highest/lowest frequency..
-
+        int arr[] = {10, 5, 10, 15, 10, 5};
+        int n = arr.length;
+        Frequency(arr, n);
     
     
     
