63338bfe5fee231b4f82ec7145f498bbc45d726a
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-06 13:44:17 +0530

    5ques

--- Day-5/PatternsCode.java
+++ Day-5/PatternsCode.java
@@ -38,6 +38,14 @@ public class PatternsCode{
         }
     }
 
+    public static void nTrianglenums(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 
 
 
@@ -55,6 +63,7 @@ public class PatternsCode{
         // nStarTriangle(n);
         // nForest(n);
             nRForest(n);
+            nTrianglenums(n);
 
 
 
