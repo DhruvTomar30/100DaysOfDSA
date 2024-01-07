6ae5b52782be8e6c5c4e438a4f24dd607e68659a
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-07 18:41:38 +0530

    7c

--- DAY- 6/Patterns2.java
+++ DAY- 6/Patterns2.java
@@ -22,7 +22,21 @@ public class Patterns2{
         }
     }
 
-    // Ne
+    // New Pattern....
+    public static void numberCrown(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            for(int j=1;j<=n-i;j++){
+            System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
 
 
 
