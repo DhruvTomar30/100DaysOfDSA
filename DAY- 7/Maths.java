6ed9bda3e5fa894328d200d2bc5f6c6aab5466ff
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-08 21:01:16 +0530

    7b

---
+++ DAY- 7/Maths.java
@@ -0,0 +1,21 @@
+public class Maths{
+    // Count of number divisible by its digits....
+    public static int countDigits(int n) {
+        int count = 0;
+        int x = n;
+        while (x > 0) {
+            int last_digit = x % 10;
+            if (last_digit != 0 && n % last_digit == 0) {
+                count++;
+            }
+            x = x / 10;
+        }
+        return count;
+    }
+
+
+    public static void main(String args[]){
+        int n=35;                           // 35- as 35 is only divisible by 5 not 3 so count is 1.
+        System.out.print(countDigits(n));   //1
+    }
+}
