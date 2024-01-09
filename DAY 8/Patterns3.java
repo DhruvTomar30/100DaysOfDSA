d397b4da28240752c68116d5e2d811773de418bf
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-09 15:37:47 +0530

    8b

--- DAY 8/Patterns3.java
+++ DAY 8/Patterns3.java
@@ -10,4 +10,21 @@ public class Patterns3 {
             System.out.println();
         }
     }
+
+
+    // 2nd Code...
+    public static void alphaRamp(int n) {
+        // Write your code here 
+        char ch='A';   
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch+" ");
+            }
+            System.out.println();
+            ch++;
+        }
+    }
+
+    // 3rd code... 
+    
 }
