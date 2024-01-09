b86edd3dc3fb21eb181a5ec68cbee5963f7a0da5
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-09 16:11:51 +0530

    8e

--- DAY 8/Patterns3.java
+++ DAY 8/Patterns3.java
@@ -45,4 +45,53 @@ public class Patterns3 {
             System.out.println();
         }
     }
+
+    // 4th code...
+    public static void symmetry(int n) {
+        // Write your code here
+        for(int i=1;i<=n;i++){
+            // stars
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("*"+" ");
+            }
+            // spaces
+            for(int j=1;j<=(2*i)-2;j++){
+                 System.out.print(" ");
+            }
+            // stars
+             for(int j=1;j<=n-i+1;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        // lower symmetry... 
+        for(int i=1;i<=n;i++){
+            // stars
+            for(int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            // spaces
+            for(int j=1;j<=(2*n-2*i);j++){
+                 System.out.print(" ");
+            }
+            // stars
+             for(int j=1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+
+
+
+
+
+
+
+
+    
+    public static void main(String args[]){
+        int n=3;
+        symmetry(n);
+    }
 }
