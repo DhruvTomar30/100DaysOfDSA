2f67469e03fffdc6315b057e69c922ad169cd236
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-09 17:07:14 +0530

    8g

--- DAY 8/Patterns3.java
+++ DAY 8/Patterns3.java
@@ -82,14 +82,61 @@ public class Patterns3 {
         }
     }
 
-
-
-
-
-
-
-
     
+	public static void symmetry(int n) {
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            for (int j = 1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for (int j = 1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+
+        for(int i =n-1;i>=1;i--){
+            for (int j =1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            for (int j = 1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for (int j = 1;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }   
+
+
+	// Hollow Rhombus..
+	public static void getStarPattern(int n) {
+        // Write your code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i==1||j==1||i==n||j==n){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+	
+
+
+
+
+
+
+
+
+
     public static void main(String args[]){
         int n=3;
         symmetry(n);
