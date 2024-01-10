8decb40de7d570b37c07a0e11a4f9963e095007d
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-10 22:06:50 +0530

    9e

--- DAY 9/Basicmath.java
+++ DAY 9/Basicmath.java
@@ -14,6 +14,24 @@ public class Basicmath{
         return ans;
     }
 
+    // All Divisors sum 
+    public static int sumOfAllDivisors(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+    public static int sumOfDivisorsInRange(int n) {
+        int totalSum = 0;
+        for (int i = 1; i <= n; i++) {
+            totalSum += sumOfAllDivisors(i);
+        }
+        return totalSum;
+    }
+
     
     
 
