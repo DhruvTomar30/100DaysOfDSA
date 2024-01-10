2003f7666df8452b402fa41adac56a0a9659fba0
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-10 18:25:05 +0530

    9c

--- DAY 9/Basicmath.java
+++ DAY 9/Basicmath.java
@@ -1,4 +1,6 @@
+import java.util.*;
 public class Basicmath{
+    
     // GCD of 2 numbers..
     public static int calcGCD(int n, int m){
         int ans=1;
@@ -13,6 +15,46 @@ public class Basicmath{
     }
 
     
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // Main....
+    public static void main(String[] args) {
+
+        // ArmString O(logn)...
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n;
+        int sum = 0;
+        int numDigits = (int) Math.log10(n) + 1;
+
+        while (n > 0) {
+            int lastDigit = n % 10;
+            int poweredDigit = (int) Math.pow(lastDigit, numDigits);
+            sum += poweredDigit;
+            n = n / 10;
+        }
+
+        if (temp == sum) {
+            System.out.print("true");
+        } else {
+            System.out.print("false");
+        }
+    }
+
+
+
 
 
 
