8c6393f31a70cd3d4c894a4cf11bcceeec1f7dfd
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-10 18:06:04 +0530

    9b

---
+++ DAY 9/BasicMaths.java
@@ -0,0 +1,14 @@
+public class BasicMaths {
+    // GCD of 2 numbers..
+    public static int calcGCD(int n, int m){
+        int ans=1;
+        for(int i=1;i<=Math.min(n,m);i++){
+            if(n%i==0 && m%i==0){
+                ans=i;
+            }
+            
+        }
+        System.out.println();
+        return ans;
+    }
+}
--- Striver/BasicMaths.java
+++ Striver/BasicMaths.java
@@ -82,14 +82,14 @@ public class BasicMaths {
 
         // <--------- GCD Greatest common factor ---------->
 
-    //   int num1 = 3, num2 = 6;
-    //   int ans = 1;
-    //   for (int i = 1; i <= Math.min(num1, num2); i++) {
-    //     if (num1 % i == 0 && num2 % i == 0) {
-    //       ans = i;
-    //     }
-    //   }
-    //   System.out.print("The GCD of the two number is "+ans);
+      int num1 = 3, num2 = 6;
+      int ans = 1;
+      for (int i = 1; i <= Math.min(num1, num2); i++) {
+        if (num1 % i == 0 && num2 % i == 0) {
+          ans = i;
+        }
+      }
+      System.out.print("The GCD of the two number is "+ans);
 
 
 
