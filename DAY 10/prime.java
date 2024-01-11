e7a7662c77445360580b5f0622c0ae630f22bdc9
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-11 23:45:32 +0530

    10g

--- DAY 10/prime.java
+++ DAY 10/prime.java
@@ -25,17 +25,4 @@ public class prime{
 	}
 }
 
-public class prime{
-	public static String isPrime(int num) {
-		if(num<=1){
-			return "NO";
-		}
-		for(int i=2;i<=Math.sqrt(num);i++){
-			if(num%i==0){
-				return "NO";
-			}
-		}
-		return "YES";
-	}
-}
 
