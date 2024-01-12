b1a8f120416408c098dc19ed75569834af1dd52d
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-12 22:40:17 +0530

    11f

--- DAY-11/IncNum.java
+++ DAY-11/IncNum.java
@@ -1,12 +1,27 @@
- public static void printName(int i, int n){
+public class IncNum{
+public static void printName(int i, int n){
         if(i>n){
             return;
         }
         System.out.print("Dhruv" + " ");
         printName(i+1, n);
+}
+
+public static void printInc(int i, int n){
+    if(i>n){
+        return;
     }
+    System.out.print(n + " ");
+    printInc(n-1);
+}
     
     public static void main(String args[]){
         int n=5;
-        printName(1,n);
+        // printName(1,n);
+
+        // Decreasing order..
+        printInc(n);
+
     }
+
+} 
