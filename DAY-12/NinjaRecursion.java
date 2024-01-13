98dfc333d95eba3f8c110e303ca4153d832095a6
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-13 17:10:32 +0530

    12b

--- DAY-12/NinjaRecursion.java
+++ DAY-12/NinjaRecursion.java
@@ -2,17 +2,32 @@ import java.util.*;
 public class NinjaRecursion {  
 
     // 01. Increasing order Code...
-    static int[] nums(int n, int[] arr){
+    static int[] IncNums(int n, int[] arr){
         if(n > arr.length) return arr;
         if(n<=0) return arr;
         int i=n-1;
         arr[i]=n;
-        return nums(n+1, arr);
+        return IncNums(n+1, arr);
         
     }
     public static int[] printNos(int x) {
         int[] arr = new int[x];
-        return nums(1, arr);
+        return IncNums(1, arr);
+    }
+
+
+    // 02. Decreasing order code..
+    static int[] DecNums(int n, int[] arr){
+        if(n > arr.length) return arr;
+        if(n<=0) return arr;
+        int i=arr.length-n;
+        arr[i]=n;
+        return DecNums(n+1, arr);
+    }
+
+    public static int[] printNumbs(int x){
+        int arr[]=new int[x];
+        return DecNums(1, arr);
     }
 
 
@@ -29,8 +44,9 @@ public class NinjaRecursion {
         int[] result = printNos(x);
         
         // 02. [5,4,3,2,1]
-         int[] result = printNos(x);
+         int[] resultt = printNumbs(x);
         System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(resultt));
 
 
     }
