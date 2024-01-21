94297d895ca3f02bdffb978d904760eb25c005da
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-21 23:28:33 +0530

    20e

--- DAY-20/NinjaSortings.java
+++ DAY-20/NinjaSortings.java
@@ -31,6 +31,20 @@ public class NinjaSortings {
         }
     }
 
+    // 03. Insertion Sort.....
+    public static void insertionSort(int[] arr, int size) {
+        //Your code goes here
+        for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+    }
+
 
 
 
