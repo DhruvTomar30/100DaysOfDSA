e3fc0d4430b006e699c05039d34eb1a5746dd621
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-21 23:24:54 +0530

    20d

--- DAY-20/NinjaSortings.java
+++ DAY-20/NinjaSortings.java
@@ -14,6 +14,25 @@ public class NinjaSortings {
             arr[minpos]=arr[i];
             arr[i]=temp;
         }
-
     }
+
+    // 02. Bubble Sort...
+    public static void bubbleSort(int[] arr, int n) {
+        //Your code goes here
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    // swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+
+
+
+
+
 }
