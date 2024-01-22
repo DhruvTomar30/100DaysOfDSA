b80cedd524eca76ecf32b169652cdb3b37593457
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-22 22:10:52 +0530

    21f

--- DAY-21/NinjaSortings.java
+++ DAY-21/NinjaSortings.java
@@ -2,7 +2,7 @@ public class NinjaSortings {
 
     // 01. Selection Sort....
     public static void selectionSort(int[] arr) {
-        for(int i=0;i<=arr.length-1;i++){
+        for(int i=0;i<arr.length-1;i++){
             int minpos=i;
             for(int j=i+1;j<arr.length;j++){
                 if(arr[minpos]>arr[j]){
@@ -14,6 +14,13 @@ public class NinjaSortings {
             arr[minpos]=arr[i];
             arr[i]=temp;
         }
+
+        // 'or' ........ 
+        for(int i=0; i<=arr.length-2;i++){
+            int minpos=i;
+            for(int j=i;j<arr.length-1;j++)
+        }
+        
     }
 
     // 02. Bubble Sort...
@@ -30,6 +37,23 @@ public class NinjaSortings {
             }
         }
     }
+
+    // 03. Insertion Sort.....
+    public static void insertionSort(int[] arr, int size) {
+        //Your code goes here
+        for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+    }
+
+
+
 
 
 }
