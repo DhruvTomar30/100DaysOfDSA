b2a9026b0d73cfaa8a9e00d7c412fa69ab9a8518
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-25 20:40:19 +0530

    24d

--- DAY-24/SortingII.java
+++ DAY-24/SortingII.java
@@ -49,7 +49,32 @@ public class SortingII{
 
 
     //02. Quick Sort... 
-
+    public static void quickSort(int[] arr,int si, int ei) {
+		if(si>=ei) return;
+		// last element
+		int pIdx=partition(arr,si,ei);
+		quickSort(arr,si,pIdx-1);	// left
+		quickSort(arr,pIdx+1, ei);	// right
+	}
+	public static int partition(int arr[], int si, int ei){
+		int pivot=arr[ei];
+		int i=si-1;  //to make place for ele. smaller than pivot
+		for(int j=si; j<ei;j++){
+			if(arr[j]<=pivot){
+				i++;
+				//swap
+				int temp=arr[j];
+				arr[j]=arr[i];
+				arr[i]=temp;
+			}
+		}
+		i++;
+		//swap
+		int temp=pivot;
+		arr[ei]=arr[i];
+		arr[i]=temp;
+		return i;
+	}
 
 
 
@@ -60,8 +85,17 @@ public class SortingII{
 
     // Main....
     public static void main(String[] args) {
+
+        // 01. Merge Sort....
         int arr[]={6,3,9,5,2,8};
-        mergeSort(arr,0,arr.length-1);
+        // mergeSort(arr,0,arr.length-1);
+        // print(arr);
+    
+
+        //02. Quick Sort...
+        quickSort(arr,0,arr.length-1);
         print(arr);
+
+
     }
 }
