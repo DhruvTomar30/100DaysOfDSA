d7a46e9ff7a636321926748043021163b45cba10
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-26 19:47:33 +0530

    25

--- DAY-25/EasyArray.java
+++ DAY-25/EasyArray.java
@@ -1,5 +1,20 @@
 public class EasyArray{
 
+    // 01. Largest Element in an Array....
+     static int largestElement(int[] arr, int n) {
+        // Write your code here.
+        int largest=Integer.MIN_VALUE;
+        for(int i=0; i<arr.length;i++){
+           if(largest<arr[i]){
+               largest=arr[i];
+           }
+        }
+        return largest;
+    }
+
+    
+    // 02.. Second largest and smallest element in an Array...
+    
 
 
 
