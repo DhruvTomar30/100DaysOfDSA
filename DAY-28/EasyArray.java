e30ca9f7ca14d225dcb73a8f1fd3636b4c9c7ef6
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-29 19:45:57 +0530

    intial

---
+++ DAY-28/EasyArray.java
@@ -0,0 +1,66 @@
+public class EasyArray{
+
+    // 01. Largest Element in an Array....
+     static int largestElement(int[] arr, int n) {
+        // Write your code here.
+        int largest=Integer.MIN_VALUE;
+        for(int i=0; i<arr.length;i++){
+           if(largest<arr[i]){
+               largest=arr[i];
+           }
+        }
+        return largest;
+    }
+
+    
+    // 02.. Second largest and smallest element in an Array...
+    public static int[] getSecondOrderElements(int n, int []arr) {
+        int secondLargest=Integer.MIN_VALUE, largest=Integer.MIN_VALUE;
+        int secondSmallest=Integer.MAX_VALUE, smallest=Integer.MAX_VALUE; 
+
+        for(int i=0;i<arr.length;i++){
+            if(largest<arr[i]){
+                secondLargest=largest;
+                largest=arr[i];
+            }
+            else if(arr[i]>secondLargest && arr[i]!=largest){
+                secondLargest=arr[i];
+            }
+        }
+
+        for(int i=0; i<arr.length;i++){
+           if(smallest>arr[i]){
+               secondSmallest=smallest;
+               smallest=arr[i];
+           }
+           else if(arr[i] < secondSmallest && arr[i] != smallest){
+               secondSmallest=arr[i];
+           }
+        }
+        int result[]={secondLargest, secondSmallest};
+        return result;
+    }
+
+
+    // 03. Remove Duplicates in the Array...
+    public static int removeDuplicates(int[] arr,int n) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+
+
+
+
+
+
+    
+    public static void main(String args[]){
+        
+    }
+}
