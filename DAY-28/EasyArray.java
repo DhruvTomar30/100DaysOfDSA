4657f79bea1298ad22583af61fe7733ff80b7dc7
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-01-29 19:53:04 +0530

    d

--- DAY-28/EasyArray.java
+++ DAY-28/EasyArray.java
@@ -55,11 +55,29 @@ public class EasyArray{
     }
 
 
-
-
-
-
+    // 04. Left Rotate an Array by one place....
     
+
+
+
+
+
+
+    // 07. Linear Search ...
+    public static int linearSearch(int n, int num, int []arr){
+        // Write your code here.
+        for(int i=0;i<n;i++){
+        if(arr[i]==num)
+            return i;
+        }
+        return -1;
+    }
+
+
+
+
+
+    // Main...... 
     public static void main(String args[]){
         
     }
