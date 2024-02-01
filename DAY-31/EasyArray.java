b0c5565e89e7c1a52f9269546ba38adbee8faf3c
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-01 22:39:48 +0530

    f

--- DAY-31/EasyArray.java
+++ DAY-31/EasyArray.java
@@ -122,7 +122,19 @@ public class EasyArray{
     }
 
     // 09. Find missing numbers in an Array... 
-    
+    public int missingNumber(int[] nums) {
+        int N = nums.length;
+        int sum = (N * (N + 1)) / 2;
+
+        // Summation of all array elements:
+        int s2 = 0;
+        for (int i = 0; i < N; i++) {
+            s2 += nums[i];
+        }
+        int missingNum = sum - s2;
+        return missingNum;
+    }
+
 
 
 
