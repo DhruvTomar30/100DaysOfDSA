41a13768bb0e4d651e64d77ddce13190f3744c92
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-04 22:57:15 +0530

    Spiral Matrix

--- DAY-34/EasyArray.java
+++ DAY-34/EasyArray.java
@@ -309,6 +309,39 @@ public class EasyArray{
         return false;
         
     }
+    // Spiral Matrix Code....
+    public static void printSpiral(int Matrix[][]) {
+        int startRow = 0;
+        int endRow = Matrix.length - 1;
+        int startCol = 0;
+        int endCol = Matrix[0].length - 1;
+
+        while (startRow <= endRow && startCol <= endCol) {
+            // Top
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(Matrix[startRow][j] + " ");
+            }
+            // Right
+            for (int i = startRow + 1; i <= endRow; i++) {
+                System.out.print(Matrix[i][endCol] + " ");
+            }
+            // Bottom
+            for (int j = endCol - 1; j >= startCol; j--) {
+                System.out.print(Matrix[endRow][j] + " ");
+            }
+            // Left
+            for (int i = endRow - 1; i >= startRow+1; i--) {
+                System.out.print(Matrix[i][startCol] + " ");
+            }
+
+            startRow++;
+            startCol++;
+            endCol--;
+            endRow--;
+        }
+        System.out.println();
+    }
+
 
 
 
