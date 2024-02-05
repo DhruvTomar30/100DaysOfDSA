19f2eab5a053c59965d074fbc5b7180bf08ea209
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-06 00:05:41 +0530

    36

--- Striver/Arrays/EasyArray.java
+++ Striver/Arrays/EasyArray.java
@@ -362,7 +362,27 @@ public class EasyArray{
 
 
     // Ques- 12 Find No. that appears once in the Array...
-    
+    public static int getSingleElement(int []arr){
+        Write your code here.
+        int result=0;
+        HashMap<Integer, Integer> map= new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i],1);
+                result=arr[i];
+            }
+        }
+        
+        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+            if(entry.getValue()==1){
+                result=entry.getKey();
+                break;
+            }
+        }
+        return result;
 
 
 
