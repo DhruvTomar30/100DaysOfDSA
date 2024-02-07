5f7607654e39916b0d3c7be9770c14936c4aa6d9
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-07 22:21:32 +0530

    2 sum code

--- DAY-37/MediumArray.java
+++ DAY-37/MediumArray.java
@@ -1,3 +1,27 @@
 public class Solution{
-    
+
+    // 01..  2 Sum Code 
+    public static String read(int n, int []arr, int target){
+        // Write your code here.
+        for(int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    // 02.. 
+
+
+
+
+
+
+
+
+    // Main .... 
+    public static void main(String args[]){
+
+    }
 }
