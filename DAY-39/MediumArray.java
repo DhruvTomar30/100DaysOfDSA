b365cae7cbbccba1a891ad5da8f9c6d12b9d4fc6
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-09 17:33:03 +0530

    2 sum code

--- DAY-39/MediumArray.java
+++ DAY-39/MediumArray.java
@@ -1,5 +1,104 @@
-public class Main{
+public class Solution{
+
+    // 01..  2 Sum Code 
+    public static String read(int n, int []arr, int target){
+        // Write your code here.
+        for(int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == target) return "YES";
+            }
+        }
+        return "NO";
+    }
+
+    // 02..   Sort An Array of 0s, 1s and 2s ..
+     public static void sortArray(ArrayList<Integer> arr, int n) {
+        // Write your code here.
+        int low = 0, mid = 0, high = n - 1; // 3 pointers
+
+        while (mid <= high) {
+            if (arr.get(mid) == 0) {
+                // swapping arr[low] and arr[mid]
+                int temp = arr.get(low);
+                arr.set(low, arr.get(mid));
+                arr.set(mid, temp);
+
+                low++;
+                mid++;
+
+            } else if (arr.get(mid) == 1) {
+                mid++;
+
+            } else {
+                // swapping arr[mid] and arr[high]
+                int temp = arr.get(mid);
+                arr.set(mid, arr.get(high));
+                arr.set(high, temp);
+
+                high--;
+            }
+        }
+    }
+
+    // 03... Majority Element (>n/2 times)..
+    public static int majorityElement(int []v) {
+        // Write your code here
+        int n = v.length;
+        int cnt = 0; // count
+        int el = 0; // Element
+
+        //applying the algorithm:
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = v[i];
+            } else if (el == v[i]) cnt++;
+            else cnt--;
+        }
+
+        //checking if the stored element
+        // is the majority element:
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (v[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) return el;
+        return -1;
+    }
+
+    // 04... 
+
+
+
+
+
+
+
+    // Main .... 
     public static void main(String args[]){
         
+        //01. 2 sum 
+        int n = 5;
+        int[] arr = {2, 6, 5, 8, 11};
+        int target = 14;
+        String ans = twoSum(n, arr, target);
+        System.out.println("This is the answer for variant 1: " + ans);
+
+        // 02. code 
+        int n = 6;
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(new Integer[] {0, 2, 1, 2, 0, 1}));
+        sortArray(arr, n);
+        System.out.println("After sorting:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.println();
+
+        // 03.. Majority element..
+         int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        int ans = majorityElement(arr);
+        System.out.println("The majority element is: " + ans);
+
     }
 }
