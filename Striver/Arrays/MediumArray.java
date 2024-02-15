d678c573b4f995935aee9d9c29e7bdba48383000
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-15 19:52:30 +0530

    Striver file

--- Striver/Arrays/MediumArray.java
+++ Striver/Arrays/MediumArray.java
@@ -214,6 +214,86 @@ public class Solution{
 
 
 
+    // 07. Rearranging the array in alternating +ve and -ve signs..
+
+    // Brute Approach... O(N + N/2)..
+    public static int[] RearrangebySign(int[] A, int n){
+    
+        ArrayList<Integer> pos=new ArrayList<>();
+        ArrayList<Integer> neg=new ArrayList<>();
+        
+        // Segregate the array into positives and negatives.
+        for(int i=0;i<n;i++){
+            
+            if(A[i]>0) pos.add(A[i]);
+            else neg.add(A[i]);
+        }
+        
+        // Positives on even indices, negatives on odd.
+        for(int i=0;i<n/2;i++){
+            
+            A[2*i] = pos.get(i);
+            A[2*i+1] = neg.get(i);
+        }
+
+        
+        return A;
+    }   
+
+    // Optimal Approach ... O(n)
+    public static ArrayList<Integer> RearrangebySign(ArrayList<Integer> A, int n) {
+        // Define 2 ArrayLists, one for storing positive 
+        // and other for negative elements of the array.
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+
+        // Segregate the array into positives and negatives.
+        for (int i = 0; i < n; i++) {
+            if (A.get(i) > 0)
+                pos.add(A.get(i));
+            else
+                neg.add(A.get(i));
+        }
+
+        // If positives are lesser than the negatives.
+        if (pos.size() < neg.size()) {
+
+            // First, fill array alternatively till the point 
+            // where positives and negatives are equal in number.
+            for (int i = 0; i < pos.size(); i++) {
+                A.set(2 * i, pos.get(i));
+                A.set(2 * i + 1, neg.get(i));
+            }
+
+            // Fill the remaining negatives at the end of the array.
+            int index = pos.size() * 2;
+            for (int i = pos.size(); i < neg.size(); i++) {
+                A.set(index, neg.get(i));
+                index++;
+            }
+        }
+
+        // If negatives are lesser than the positives.
+        else {
+            // First, fill array alternatively till the point 
+            // where positives and negatives are equal in number.
+            for (int i = 0; i < neg.size(); i++) {
+                A.set(2 * i, pos.get(i));
+                A.set(2 * i + 1, neg.get(i));
+            }
+
+            // Fill the remaining positives at the end of the array.
+            int index = neg.size() * 2;
+            for (int i = neg.size(); i < pos.size(); i++) {
+                A.set(index, pos.get(i));
+                index++;
+            }
+        }
+        return A;
+    }
+
+    
+
 
 
 
