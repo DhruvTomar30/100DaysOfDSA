c788e048e0edb695c87bfd21f25a43d7cef32f0a
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-16 23:06:23 +0530

    Another file commit

--- Striver/Arrays/MediumArray.java
+++ Striver/Arrays/MediumArray.java
@@ -294,6 +294,30 @@ public class Solution{
 
     // 08. Next Permutation....
 
+    // 09. Superior Element (Leaders in Array)...
+    public static List< Integer > superiorElements(int []arr) {
+        // Write your code here.
+                // printLeadersBruteForce(int[] arr, int n){
+                    int n=arr.length;
+    
+        ArrayList<Integer> ans= new ArrayList<>();
+        
+        // Last element of an array is always a leader,
+        // push into ans array.
+        int max = arr[n - 1];
+
+            ans.add(arr[n-1]);
+            
+            // Start checking from the end whether a number is greater
+            // than max no. from right, hence leader.
+            for (int i = n - 2; i >= 0; i--)
+            if (arr[i] > max) {
+                ans.add(arr[i]);
+                max = arr[i];
+            }
+
+        return ans;
+    }
 
 
 
