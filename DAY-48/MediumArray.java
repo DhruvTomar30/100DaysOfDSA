41a98943cb542d5144fd0fe7c08cbb12ac79b0ce
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-18 18:42:02 +0530

    Better Approach of 0 matrix

--- DAY-48/MediumArray.java
+++ DAY-48/MediumArray.java
@@ -337,7 +337,33 @@ public class Solution{
         }
         return maxcnt;
     }
-    
+    // 11. zero matrix..
+     // 11. Zero Matrix...
+    public static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, Integer n, Integer m) {
+    	// Write your code here.
+        int[] row = new int[n]; // row array
+        int[] col = new int[m]; // col array
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    // mark ith index of row wih 1:
+                    row[i] = 1;
+
+                    // mark jth index of col wih 1:
+                    col[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix.get(i).set(j, 0);
+                }
+            }
+        }
+        return matrix;
+    }
+
     
 
 
