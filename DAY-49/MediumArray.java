621c8941fef75a01353d621edc06f758d5ef904b
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-19 18:00:29 +0530

    Next Permutation Optimal Code

--- DAY-49/MediumArray.java
+++ DAY-49/MediumArray.java
@@ -292,7 +292,46 @@ public class Solution{
         return A;
     }
 
-    // 08. Next Permutation..
+    // 08. Next Permutation.. 
+    public void nextPermutation(int[] nums) {
+        int n=nums.length;
+        int k=n-2;
+        // null checks..
+        if(n==0 || nums==null) return;
+        // find 3 1 4 (4) 2
+        for(int i=n-1;i>0;i--){
+            if(nums[i]<=nums[i-1]) 
+                k--;
+            else {
+               break;
+            }
+        }
+        // if k==-1 reverse..
+        if(k==-1) {
+            reverse(nums,0,n-1);
+            return;
+        }
+        // replace k with next greater element reverse(reset)
+        for(int i=n-1;i>0;i--){
+            if(nums[i]>nums[k]){
+                // swap.
+                int temp=nums[k];
+                nums[k]=nums[i];
+                nums[i]=temp;
+                break;
+            }
+        }
+        reverse(nums,k+1, n-1);
+    }
+    void reverse(int nums[], int start, int end){
+        while(start<end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
     
 
     // 09.  Superior Elements ...
