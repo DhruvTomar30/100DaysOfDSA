5763c948671758a174c7cbe49cda7510e0497857
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-20 22:48:02 +0530

    changes

--- Striver/Arrays/MediumArray.java
+++ Striver/Arrays/MediumArray.java
@@ -408,6 +408,27 @@ public class Solution{
     }
 
 
+    // 13. Rotate the Matrix....
+     public static void rotateMatrix(int [][]matrix){
+        // Write your code here.
+         for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+
 
 
     // Main .... 
