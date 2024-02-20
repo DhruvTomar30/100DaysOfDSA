3168419266c7fed4facc5ab66659871c516473f9
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-20 22:51:03 +0530

    brute code of Rotated Array..

--- DAy-50/MediumArray.java
+++ DAy-50/MediumArray.java
@@ -405,7 +405,6 @@ public class Solution{
     }
 
     // 13. Rotate the Matrix....
-
      public static void rotateMatrix(int [][]matrix){
         // Write your code here.
          for (int i = 0; i < matrix.length; i++) {
