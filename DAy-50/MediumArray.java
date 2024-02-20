c32f2e8c96b74d97d6f3dd13b63903e387ed41aa
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-20 22:50:24 +0530

    optimal code of Matrix

--- DAy-50/MediumArray.java
+++ DAy-50/MediumArray.java
@@ -404,6 +404,28 @@ public class Solution{
         return matrix;
     }
 
+    // 13. Rotate the Matrix....
+
+     public static void rotateMatrix(int [][]matrix){
+        // Write your code here.
+         for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+    
     
 
 
