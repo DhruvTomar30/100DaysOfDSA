1b422b3e27003d774ab48062f1187261202e0c25
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-02-25 21:31:53 +0530

    Brutel code of majority

---
+++ DAY-55/HardArray.java
@@ -0,0 +1,60 @@
+public class Main{
+
+    // 01.. Pascal's Triangle..... (variant 1)....
+    public static long nCr(int n, int r) {
+        long res = 1;
+
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+
+    public static int pascalTriangle(int r, int c) {
+        int element = (int) nCr(r - 1, c - 1);
+        return element;
+    }
+
+    // Variant 2.... 
+    public static long nCr(int n, int r) {
+        long res = 1;
+
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+
+    public static void pascalTriangle(int n) {
+        // printing the entire row n:
+        for (int c = 1; c <= n; c++) {
+            System.out.print(nCr(n - 1, c - 1) + " ");
+        }
+        System.out.println();
+    }
+
+
+    // 02. Majority Element (n/3)....
+    
+
+
+
+
+
+    // Main.....
+    public static void main(String[] args) {
+
+        // 01. Pascal's Triangle..
+        int r = 5; // row number
+        int c = 3; // col number
+        int element = pascalTriangle(r, c);
+        System.out.println("The element at position (r,c) is: " + element);
+
+        // Variant 2.. 
+        pascalTriangle(n);
+    }
+}
