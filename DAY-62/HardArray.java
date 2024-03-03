75dc170fa3736c440cfd3d776aa68410ff42dde5
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-03-03 17:25:05 +0530

    ..

--- DAY-62/HardArray.java
+++ DAY-62/HardArray.java
@@ -24,7 +24,6 @@ public class Main{
     // optimal Approaches variant 1....
     public static long nCr(int n, int r) {
         long res = 1;
-
         // calculating nCr:
         for (int i = 0; i < r; i++) {
             res = res * (n - i);
