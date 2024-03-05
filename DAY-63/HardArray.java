cae907c9b3c7476ebc7864fd35eef5520ce1dd2f
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-03-05 17:52:43 +0530

    brute of 3 sum..

--- DAY-63/HardArray.java
+++ DAY-63/HardArray.java
@@ -135,30 +135,27 @@ public class Main{
 
 
     // Ques-03   3 Sum code... 
-    // Better Code..
-     public static List<List<Integer>> triplet(int n, int[] arr) {
-        Set<List<Integer>> st = new HashSet<>();
+    // Better Approach.... 
+    public static List< List < Integer > > triplet(int n, int []arr) {
+       
+         Set<List<Integer>> st = new HashSet<>();
 
+        // check all possible triplets:
         for (int i = 0; i < n; i++) {
-            Set<Integer> hashset = new HashSet<>();
             for (int j = i + 1; j < n; j++) {
-                //Calculate the 3rd element:
-                int third = -(arr[i] + arr[j]);
-
-                //Find the element in the set:
-                if (hashset.contains(third)) {
-                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
-                    temp.sort(null);
-                    st.add(temp);
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[k]);
+                        temp.sort(null);
+                        st.add(temp);
+                    }
                 }
-                hashset.add(arr[j]);
             }
         }
-
-        // store the set elements in the answer:
-        List<List<Integer>> ans = new ArrayList<>(st);
-        return ans;
+        return st;
     }
+
+    // Optimal code.. 
 
 
 
