8856926593f7a424d015d6632bfabf24639b9a83
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-03-06 23:18:50 +0530

    ..

--- DAY-64/HardArray.java
+++ DAY-64/HardArray.java
@@ -295,6 +295,37 @@ public class Main{
     }
 
 
+    // Merge Overlapping Sub-intervals... 
+    public static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
+        int n = arr.length; // size of the array
+        //sort the given intervals:
+        Arrays.sort(arr, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) { // select an interval:
+            int start = arr[i][0];
+            int end = arr[i][1];
+
+            //Skip all the merged intervals:
+            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)) {
+                continue;
+            }
+            //check the rest of the intervals:
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j][0] <= end) {
+                    end = Math.max(end, arr[j][1]);
+                } else {
+                    break;
+                }
+            }
+            ans.add(Arrays.asList(start, end));
+        }
+        return ans;
+    }
+
     // Main.....
     public static void main(String[] args) {
 
