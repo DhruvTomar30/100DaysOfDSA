b46021914da718fa2da595fe61678004c4f9a0b7
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-03-13 15:18:07 +0530

    ..

--- DAY-72/HardArray.java
+++ DAY-72/HardArray.java
@@ -386,7 +386,8 @@ public class Main{
                 arr1[i] = arr3[i];
             } else {
                 arr2[i - n] = arr3[i];
-            }}
+            }
+        }
     }
 
     
@@ -417,6 +418,36 @@ public class Main{
         Arrays.sort(arr1);
         Arrays.sort(arr2);
     }
+
+    // Find the repeating and missing numbers
+    // Brute Approach..
+    
+    public static int[] findMissingRepeatingNumbers(int[] a) {
+        int n = a.length; // size of the array
+        int repeating = -1, missing = -1;
+
+        //Find the repeating and missing number:
+        for (int i = 1; i <= n; i++) {
+            //Count the occurrences:
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (a[j] == i) cnt++;
+            }
+
+            if (cnt == 2) repeating = i;
+            else if (cnt == 0) missing = i;
+
+            if (repeating != -1 && missing != -1)
+                break;
+        }
+        int[] ans = {repeating, missing};
+        return ans;
+    }
+
+
+
+
+
 
 
     // Main.....
