9eb0a1a2167c0412f25bf0133de6af80bae500b0
Dhruv Tomar <dhruvtomar2020@gmail.com>
2024-03-13 15:20:05 +0530

    ..

--- Striver/Arrays/HardArray.java
+++ Striver/Arrays/HardArray.java
@@ -386,7 +386,8 @@ public class Main{
                 arr1[i] = arr3[i];
             } else {
                 arr2[i - n] = arr3[i];
-            }}
+            }
+        }
     }
 
     
@@ -417,6 +418,58 @@ public class Main{
         Arrays.sort(arr1);
         Arrays.sort(arr2);
     }
+
+    // Find the repeating and missing numbers
+    // Brute Approach..
+
+    public static int[] findMissingRepeatingNumbers(int[] a) {
+        int n = a.length; // size of the array
+        int repeating = -1, missing = -1;
+
+        //Find the repeating and missing number:
+        for (int i = 1; i <= n; i++) {
+            //Count the occurrences:
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (a[j] == i) cnt++;
+            }
+
+            if (cnt == 2) repeating = i;
+            else if (cnt == 0) missing = i;
+
+            if (repeating != -1 && missing != -1)
+                break;
+        }
+        int[] ans = {repeating, missing};
+        return ans;
+    }
+
+
+    // optimal Approach....
+    public static int[] findMissingRepeatingNumbers(int[] a) {
+        int n = a.length; // size of the array
+        int[] hash = new int[n + 1]; // hash array
+
+        //update the hash array:
+        for (int i = 0; i < n; i++) {
+            hash[a[i]]++;
+        }
+
+        //Find the repeating and missing number:
+        int repeating = -1, missing = -1;
+        for (int i = 1; i <= n; i++) {
+            if (hash[i] == 2) repeating = i;
+            else if (hash[i] == 0) missing = i;
+
+            if (repeating != -1 && missing != -1)
+                break;
+        }
+        int[] ans = {repeating, missing};
+        return ans;
+    }
+
+
+
 
 
     // Main.....
